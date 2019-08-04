3571c5e4c248ef21386917deb377b384569b6fdc
Andrew McNab <andrew.mcnab@cern.ch>
2019-08-04 13:00:20 +0100

    Remove unused cvmfs config

--- pilot/include_vm_00.sh
+++ pilot/include_vm_00.sh
@@ -1,28 +1,6 @@
-##
-## In config-egi:
-## Added for SKA/LOFAR
-##
-#cat <<EOF >/etc/cvmfs/config.d/softdrive.nl.conf
-#CVMFS_SERVER_URL='http://cvmfs01.nikhef.nl/cvmfs/@fqrn@;http://cvmfs-egi.gridpp.rl.ac.uk:8000/cvmfs/@fqrn@'
-#CVMFS_PUBLIC_KEY=/etc/cvmfs/keys/softdrive.nl.pub
-#EOF
-#
-#cat <<EOF >/etc/cvmfs/keys/softdrive.nl.pub
-#-----BEGIN PUBLIC KEY-----
-#MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA481/kCXbrVtLuzcFZ2uO
-#EmiAKx28qXIkonPwr/gSmqQ8k1zQA7dKK5YZwZSbVwgYqvhvW6i3vKWLGVDj+elH
-#1u8uumPzzlAJHrS1XoR8rY4xUULjQBvV9HuJxE6OK4ZEZPvQmeGmjXd446c8J5cv
-#BQFtaonRnrxAbtO+Z0KtzsNOzBNFegu9z+lT7/fxV17Qh10w5IKQjm/v6jPdj1ME
-#CrG4QW2S9+Y+7YzbRP5QYaE4cl5cBI3Yb048ufgLJMfX3++uqwGM+rqNs/CzHvsW
-#dO6Jznr9EbzqbIrTsFeUThNmsGPObxOT3VmB0BTTjrZSYjgf8oEE4hdhgNQgh7vs
-#OwIDAQAB
-#-----END PUBLIC KEY-----
-#EOF
-
-cat <<EOF >/etc/cvmfs/config.d/config-egi.egi.eu.conf 
-CVMFS_SERVER_URL="http://cvmfs-egi.gridpp.rl.ac.uk:8000/cvmfs/@fqrn@;http://klei.nikhef.nl:8000/cvmfs/@fqrn@;http://cvmfsrepo.lcg.triumf.ca:8000/cvmfs/@fqrn@;http://cvmfsrep.grid.sinica.edu.tw:8000/cvmfs/@fqrn@;http://cvmfs-stratum-one.ihep.ac.cn:8000/cvmfs/@fqrn@"
-CVMFS_KEYS_DIR=/etc/cvmfs/keys/egi.eu
-CVMFS_FALLBACK_PROXY="http://cvmfsbproxy.cern.ch:3126;http://cvmfsbproxy.fnal.gov:3126"
+# Enable config-egi.egi.eu
+cat <<EOF >/etc/cvmfs/default.d/60-egi.conf
+CVMFS_CONFIG_REPOSITORY=config-egi.egi.eu
 EOF
 
 # Apply the CernVM-FS configuration changes
